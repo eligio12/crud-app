7b3b3ba9b4a158784c813c02f568c96559aaf99b
Luis Cortes <71467212+eligio12@users.noreply.github.com>
2023-10-30 14:03:33 -0600

    Solución Error

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const userRouter = require('./user.router');
 const router = express.Router();
 
 // colocar las rutas aquí
-router.use('users', userRouter)
+router.use('/users', userRouter)
 
 
 module.exports = router;
